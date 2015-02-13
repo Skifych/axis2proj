18d0243679b71da0f793c9b179c40247f998defd
Letnab <skif@ua.fm>
2015-02-13 21:10:21 +0400

    User+user roles. JPQL starting

--- axis2proj/src/ru/test/novoros/art/correct/db/CommandLineClient.java
+++ axis2proj/src/ru/test/novoros/art/correct/db/CommandLineClient.java
@@ -1,9 +1,12 @@
 package ru.test.novoros.art.correct.db;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
+import javax.persistence.Query;
 
 public class CommandLineClient {
 	private static final String PERSISTENCE_UNIT_NAME = "axis2proj";
@@ -25,6 +28,45 @@ public class CommandLineClient {
 		  }
 		 *  
 		 */
+	}
+	
+	
+	
+	private static void createUsers(String name, String pass, String role, String comment) {
+		//Set<Author> authorSet = new HashSet<Author>();
+		Set<UserRoles> userRoles = new HashSet<UserRoles>();
+		Users user = new Users();
+		UserRoles userRole = new UserRoles();
+		user.setLogin(name);
+		user.setPass(pass);
+		if (findUserRoleByType(role)!=null) {
+			userRole=findUserRoleByType(role);
+		}
+		else {
+			createUserRole(role);
+		}
+		user.setUserRole(userRole);
+		//Author author = new Author();
+		//Book book = new Book();
+		//au.setId(1L);
+		//g.setMessage("hello, createAndRead");
+		em.getTransaction().begin();
+		em.persist(user);
+		em.getTransaction().commit();
+	}
+	
+	private static UserRoles findUserRoleByType(String role) {
+		Query query = em.createQuery("select u from userroles u where role = :role", UserRoles.class);
+		query.setParameter("role", role);
+		return (UserRoles) query.getSingleResult();
+	}
+	private static UserRoles createUserRole(String role){
+		UserRoles userRole = new UserRoles();
+		userRole.setRole(role);
+		em.getTransaction().begin();
+		em.persist(userRole);
+		em.getTransaction().commit();
+		return findUserRoleByType(role);
 	}
 	
 	private void initEntityManager() {
